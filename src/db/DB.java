17dfa59fec22b9d27307bf71b7e364a10cfee520
Grey <gabrielnascimentosouza11@gmail.com>
2025-08-04 09:43:18 -0300

    Seller entity

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,7 +1,8 @@
 package db;
 
 import java.io.FileInputStream;
-import java.io.IOException;
+
+import java.io.IOException;	 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;

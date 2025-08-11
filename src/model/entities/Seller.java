cd1a70764e831419db3257360d414ff9e8794cfe
Grey <gabrielnascimentosouza11@gmail.com>
2025-08-11 14:24:17 -0300

    findByDep imp

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -99,4 +99,6 @@ public class Seller implements Serializable {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
+
+	
 }
